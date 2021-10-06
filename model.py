58c2077a3c9ed53cd80286605cac152a6db53fc9
comar <saywooong@gmail.com>
2021-10-06 20:15:14 +0900

    update: trivial

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.transforms as transforms
 
-__all__ = ['VGG16Perceptual']
+__all__ = ['VGG16Perceptual', 'EncoderF']
 
 
 class VGG16Perceptual():
@@ -47,3 +49,100 @@ class VGG16Perceptual():
             loss += feat1.sub(feat2).pow(2).mean()
 
         return loss / len(self.idx_targets)
+
+
+class EncoderF(nn.Module):
+    def __init__(self, nc=128, cls_ch=128, k_sz=3):
+        super().__init__()
+        self.layers = []
+        self.input_conv = nn.Conv2d(3, 1 * nc, k_sz, 1, 1)  # 256, 256, 128
+        self.block1 = SimpleBlock(1 * nc, 2 * nc, cls_ch, k_sz)  # 128, 128, 256
+        self.block2 = SimpleBlock(2 * nc, 4 * nc, cls_ch, k_sz)  # 64,  64, 512
+        self.block3 = SimpleBlock(4 * nc, 8 * nc, cls_ch, k_sz)  # 32,  32, 1024
+        self.block4 = SimpleBlock(8 * nc, 8 * nc, cls_ch, k_sz)  # 16,  16, 1024
+
+    def forward(self, x, c):
+        if len(x.shape) == 3:
+            x = x.unsqueeze(0)
+
+        x = F.relu(self.input_conv(x), True)  # 256, 256, 128
+        x = F.avg_pool2d(x, [2, 2])          # 128, 128
+        x = F.relu(self.block1(x, c), True)  # 128, 128, 256
+        x = F.avg_pool2d(x, [2, 2])  # 64,  64
+        x = F.relu(self.block2(x, c), True)  # 64,  64, 512
+        x = F.avg_pool2d(x, [2, 2])  # 32,  32
+        x = F.relu(self.block3(x, c), True)  # 32,  32, 1024
+        x = F.avg_pool2d(x, [2, 2])  # 16,  16
+        x = F.relu(self.block4(x, c), True)  # 16,  16, 1024
+        return x
+
+
+class AdaptiveGroupNorm(nn.Module):
+    __constants__ = ['num_groups', 'num_channels', 'eps', 'affine', 'weight',
+                     'bias']
+
+    def __init__(self, num_channels, cond_dim, num_groups=None, eps=1e-5, affine=False):
+        super(AdaptiveGroupNorm, self).__init__()
+        self.num_channels = num_channels
+        if num_groups is None:
+            num_groups = 32
+        self.num_groups = num_groups
+        self.eps = eps
+        self.affine = affine
+        self.fc_gamma = nn.Linear(cond_dim, self.num_channels)
+        self.fc_beta = nn.Linear(cond_dim, self.num_channels)
+        if self.affine:
+            self.weight = nn.Parameter(torch.Tensor(
+                num_channels), requires_grad=True)
+            self.bias = nn.Parameter(torch.Tensor(
+                num_channels), requires_grad=True)
+            nn.init.uniform_(self.weight)
+            nn.init.zeros_(self.bias)
+        else:
+            self.register_parameter('weight', None)
+            self.register_parameter('bias', None)
+
+    def forward(self, x, c):
+        # Normalize
+        x = F.group_norm(x, self.num_groups, self.weight, self.bias, self.eps)
+
+        # Condition it
+        gamma = 1.0 + self.fc_gamma(c)  # 1 centered
+        beta = self.fc_beta(c)
+        gamma = gamma.view(gamma.size(0), gamma.size(1), 1, 1)
+        beta = beta.view(beta.size(0), beta.size(1), 1, 1)
+
+        if self.affine:
+            # learned affine (unnecessary since it can be learned but seems to help)
+            weight = self.weight.view(1, -1, 1, 1).repeat(x.size(0), 1, 1, 1)
+            bias = self.bias.view(1, -1, 1, 1).repeat(x.size(0), 1, 1, 1)
+            gamma = gamma + weight
+            beta = beta + bias
+
+        return (gamma * x) + beta
+
+
+class SimpleBlock(nn.Module):
+    def __init__(self, in_ch, out_ch, cls_ch, k_sz):
+        super().__init__()
+        self.norm1 = AdaptiveGroupNorm(in_ch, cls_ch)
+        self.conv1 = nn.Conv2d(in_ch, out_ch // 2, k_sz, 1, 1)
+        self.norm2 = AdaptiveGroupNorm(out_ch // 2, cls_ch)
+        self.conv2 = nn.Conv2d(out_ch // 2, out_ch, k_sz, 1, 1)
+        if in_ch != out_ch:
+            self.conv3 = nn.Conv2d(in_ch, out_ch, k_sz, 1, 1)
+        return
+
+    def forward(self, x, c):
+        r = self.norm1(x, c)
+        r = F.relu(r)
+        r = self.conv1(r)
+
+        r = self.norm2(r, c)
+        r = F.relu(r, True)
+        r = self.conv2(r)
+
+        if hasattr(self, 'conv3'):
+            x = self.conv3(x)
+        return x + r
+
---
+++ train_encoder_f.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.transforms import ToPILImage, ToTensor
+import torchvision.transforms as transforms
+from pytorch_pretrained_biggan import (BigGAN, one_hot_from_int,
+        truncated_noise_sample)
+from tqdm import tqdm
+
+import os
+import os.path
+import argparse
+from torchvision.utils import make_grid
+
+from model import VGG16Perceptual, EncoderF
+import numpy as np
+import random
+
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+
+
+DEV = 'cuda'
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--seed', type=int, default=1)
+    # 5 --> 32, 4 --> 16, ...
+    parser.add_argument('--num_feat_layer', type=int, default=4)
+    parser.add_argument('--resolution', type=str, default='256')
+    parser.add_argument('--class_index', type=int, default=15)
+    parser.add_argument('--num_epoch', type=int, default=2)
+    parser.add_argument('--interval_save', type=int, default=10)
+    parser.add_argument('--size_batch', type=int, default=16)
+    parser.add_argument('--truncation', type=float, default=1.0)
+    parser.add_argument('--show', action='store_false')
+
+    # I/O
+    parser.add_argument('--path_history', type=str, default='inversion_zf_hist')
+    parser.add_argument('--path_dataset', type=str, default='dataset_encoder')
+
+    # Loss
+    parser.add_argument('--loss_mse', action='store_true', default=True)
+    parser.add_argument('--loss_lpips', action='store_true', default=False)
+    return parser.parse_args()
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+
+
+def main(args):
+    print(args)
+
+    if not os.path.exists(args.path_history):
+        os.mkdir(args.path_history)
+
+    if args.seed >= 0:
+        set_seed(args.seed)
+
+    x = torch.randn(1, 3, 256, 256).to(DEV)
+    # Logger
+    writer = SummaryWriter('runs/encoder_f_feat%02d' % args.num_feat_layer)
+
+    # Model
+    name_model = 'biggan-deep-%s' % (args.resolution)
+    model = BigGAN.from_pretrained(name_model)
+    model.to(DEV)
+    if args.loss_lpips:
+        vgg_per = VGG16Perceptual()
+    encoder = EncoderF().to(DEV)
+
+    # Latents
+    class_vector = one_hot_from_int([args.class_index],
+            batch_size=args.size_batch)
+    class_vector = torch.from_numpy(class_vector)
+    class_vector = class_vector.to(DEV)
+    with torch.no_grad():
+        embd = model.embeddings(class_vector)
+
+    # Optimizer
+    optimizer = optim.Adam(encoder.parameters())
+
+    # Datasets
+    prep = transforms.Compose([
+            ToTensor(),
+            transforms.Resize(256),
+            transforms.CenterCrop(256),
+            ])
+    dataset = ImageFolder(args.path_dataset, transform=prep)
+    dataloader = DataLoader(dataset, batch_size=args.size_batch, shuffle=True,
+            num_workers=8, drop_last=True)
+
+    # Fix valid
+    # a = next(dataloader)
+    x_test, _ = next(iter(dataloader))
+    grid_init = make_grid(x_test, nrow=4)
+    print(grid_init.shape)
+    writer.add_image('GT', grid_init)
+    exit()
+
+    # Loss
+    loss_fn = nn.MSELoss()
+
+    for epoch in range(args.num_epoch):
+        for i, (x, _) in enumerate(tqdm(dataloader)):
+
+            x = x.to(DEV)
+
+            # sample z
+            noise_vector = truncated_noise_sample(truncation=args.truncation,
+                    batch_size=args.size_batch)
+            noise_vector = torch.from_numpy(noise_vector)
+            noise_vector = noise_vector.to(DEV)
+
+            f = encoder(x, embd)
+            output = model.forward_from(noise_vector, class_vector,
+                    args.truncation, f, args.num_feat_layer)
+            output = output.add(1).div(2)
+
+            loss = loss_fn(x, output)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if i % args.interval_save == 0:
+                # writer.add_image('recon', output.squeeze(0), i)
+                writer.add_scalar('total', loss.item(), i)
+
+
+if __name__ == '__main__':
+    args = parse()
+    main(args)
